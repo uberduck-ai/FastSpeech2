521c2fd9afdec533e722e975dc3d7eb27ec45abf
Samson Koelle <sjkoelle@gmail.com>
2023-01-19 12:10:42 -0500

    parsage

--- samsparse.py
+++ samsparse.py
@@ -1,6 +1,7 @@
 import textgrid
 import os
 import pandas as pd
+import numpy as np 
 
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
@@ -9,12 +10,15 @@ def intersection(lst1, lst2):
 filelist_path  ="/usr/src/app/uberduck_local/ml/filelist.txt"
 data = pd.read_csv(filelist_path, sep="|", header=None, index_col = None)
 files_2 = os.listdir("/tmp/asdf_output")
-files = list(data[0])
-files = [f"{file.split('/')[-1].split('.')[0]}.TextGrid" for file in files]
-files = intersection(files, files_2)
+files_raw = list(data[0])
+files_smol = [file.split('/')[-1] for file in files_raw]
+files_textgrid = [f"{file.split('.')[0]}.TextGrid" for file in files_smol]
+files = intersection(files_textgrid, files_2)
 duration_data = []
 phoneme_data = []
 start_data = []
+path_data = []
+text_data= []
 for file in files:
     start_frames = [0]
     tg = textgrid.TextGrid.fromFile(f"/tmp/asdf_output/{file}")
@@ -32,21 +36,23 @@ for file in files:
     duration_data.append(duration_datum)
     phoneme_data.append(phoneme_datum)
     start_data.append(" ".join(start_frames))
+    text_data.append(data.iloc[np.where(np.asarray(files_textgrid) == file)[0][0]][1])
+    path_data.append(data.iloc[np.where(np.asarray(files_textgrid) == file)[0][0]][0])
 
 paths = []
 for i, datum in data.iterrows():
     path = datum[0].split("/")[-1]
     paths.append(path)
 
-text_data = list(data[1])
-path_data = list(data[0])
+# text_data = list(data[1])
+# path_data = list(data[0])
 
 fastspeech_data = pd.DataFrame([text_data, start_data, duration_data, phoneme_data, paths]).transpose()
-fastspeech_data.iloc[:1].to_csv("/tmp/fastspeech_data.txt", index=None, header = None, sep="|")
+fastspeech_data.iloc[:100].to_csv("/tmp/fastspeech_data.txt", index=None, header = None, sep="|")
 
 
-fastspeech_data_gimp = fastspeech_data.iloc[6:7].copy()
-fastspeech_data_gimp.to_csv("/tmp/fastspeech_data.txt", index=None, header = None, sep="|")
+# fastspeech_data_gimp = fastspeech_data.iloc[6:7].copy()
+# fastspeech_data_gimp.to_csv("/tmp/fastspeech_data.txt", index=None, header = None, sep="|")
 
 
 
